679eb71a91cd16e6b1a80ddcadebe6a3f496365e
xena <74474948+a-andres1@users.noreply.github.com>
2021-05-09 18:04:33 -0400

    added login js

--- models/index.js
+++ models/index.js
@@ -2,3 +2,8 @@ const User = require ('./User');
 const Post = require ('./Post');
 const Comment = require ('./Comment');
 
+module.exports = {
+    User: require ('./User'),
+    Post: require ('./Post'),
+    Comment: require ('./Comment')
+}
