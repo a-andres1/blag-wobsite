8911fe5d48e780fcb9e924133159e35c76fb7348
xena <74474948+a-andres1@users.noreply.github.com>
2021-05-09 18:10:36 -0400

    created comment model

--- models/Comment.js
+++ models/Comment.js
@@ -3,4 +3,42 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
-Comment.init()
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        prompt_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'prompt',
+                key: 'id',
+            },
+        },
+     
+    }, 
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+   
+)
